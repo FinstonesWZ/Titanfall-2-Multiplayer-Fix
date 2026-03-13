b82b9a437e595ee691b5e05cdab21ea30ccaf5bb
FinWZ <77031008+FinstonesWZ@users.noreply.github.com>
2026-03-13 04:50:40 +0200

    Edit user instructions
    Edited the user instructions inside Titanfall2.bat.

--- Titanfall2.bat
+++ Titanfall2.bat
@@ -1,8 +1,8 @@
 @echo off
-:: Fixes the Intel OpenSSL SHA bug for Titanfall 2 Multiplayer
+:: Fixes the Intel OpenSSL SHA bug for Titanfall 2 Multiplayer.
 set OPENSSL_ia32cap=:~0x200000200000000
 
-:: Launches the game. Adjust the path and drive letter according to your install location.
-start "" "C:\yourinstallocationhere\Titanfall2.exe" -fullscreen
+:: Launches the game. Adjust the path and drive letter according to your game install location.
+start "" "C:\yourinstallationpath\Titanfall2.exe" -fullscreen
 
 exit

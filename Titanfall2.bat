c66c6af29dce6205de6b36dc6c068b84b8b7f56e
FinWZ <77031008+FinstonesWZ@users.noreply.github.com>
2026-03-13 03:52:09 +0200

    Create Titanfall2.bat
    Fixes the bug allowing to play Multiplayer without setting OPENSSL_ia32cap on system wide level.

---
+++ Titanfall2.bat
@@ -0,0 +1,8 @@
+@echo off
+:: Fixes the Intel OpenSSL SHA bug for Titanfall 2 Multiplayer
+set OPENSSL_ia32cap=:~0x200000200000000
+
+:: Launches the game. Adjust the path and drive letter according to your install location.
+start "" "C:\yourinstallocationhere" -fullscreen
+
+exit
